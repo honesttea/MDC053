16df8de9502bb1a667721dc310f574c77584842b
João Marcelo <62082022+honesttea@users.noreply.github.com>
2020-12-01 21:12:37 -0300

    Create questao5_2.sql

---
+++ Atividades/Atividade05/questao5_2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Pessoa(
+  id INT NOT NULL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Endereco(
+  logradouro VARCHAR(100),
+  numero INT DEFAULT 0,
+  cep CHAR(0) NOT NULL,
+  cidade VARCHAR(30),
+  uf CHAR NOT NULL,
+  id INT NOT NULL FOREIGN KEY REFERENCES Pessoa(id)
+);
+
+CREATE TABLE Fisica(
+  sexo CHAR(1),
+  cpf CHAR(11) NOT NULL,
+  id INT NOT NULL FOREIGN KEY REFERENCES Pessoa(id)
+);
+
+CREATE TABLE Juridica(
+  sexo CHAR(1),
+  cnpj CHAR (14) NOT NULL,
+  id INT NOT NULL FOREIGN KEY REFERENCES Pessoa(id)
+);
